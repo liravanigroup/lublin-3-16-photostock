4938bcd589626c9034b986196c629b2d74735a5c
SlawekSobotka <ssobot@gmail.com>
2016-04-05 19:11:47 +0200

    zmiana

--- src/pl/com/bottega/photostock/sales/model/Money.java
+++ src/pl/com/bottega/photostock/sales/model/Money.java
@@ -4,19 +4,39 @@ package pl.com.bottega.photostock.sales.model;
  * Created by Slawek on 12/03/16.
  */
 public class Money {
-    private double value;
-    private String currency;
+    private final Double value = null;//TODO zamienić na Fraction
+    private final String currency = null;//TODO zamienić na użycie klasy Currency z biblioteki Javy
 
-    public Money(double value, String currency) {
-        this.value = value;
-        this.currency = currency;
+    public Money(Double value, String currency) {
+        //TODO
+    }
+
+    public Money(int integerValue, int cents, String currency) {
+        //TODO
+
     }
 
     public Money(double value) {
         this(value, "PLN");
     }
 
-    public Money() {
-        this(0);
+    public Money add(Money amount){
+        return null;//TODO
+    }
+
+    public Money substract(Money amount){
+        return null;//TODO
+    }
+
+    public Money multiple(int ratio){
+        return null;//TODO
+    }
+
+    public Money multiple(double ratio){
+        return null;//TODO
+    }
+
+    public boolean equals(Object obj){
+        return false;//TODO
     }
 }
