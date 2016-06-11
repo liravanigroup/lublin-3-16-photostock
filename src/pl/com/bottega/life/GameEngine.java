95147561881d54f959b324439c51312e0ff266ed
Maciej Zubala <maciej.zubala@gmail.com>
2016-06-11 20:30:11 +0200

    Game of life

---
+++ src/pl/com/bottega/life/GameEngine.java
@@ -0,0 +1,82 @@
+package pl.com.bottega.documentmanagement.life;
+
+/**
+ * Created by maciuch on 11.06.16.
+ */
+public class GameEngine {
+
+    private boolean[][] currentState;
+    private boolean[][] newState;
+    private int width, height;
+    private double liveRatio;
+
+    public GameEngine(int width, int height, double liveRatio) {
+        currentState = new boolean[height][width];
+        newState = new boolean[height][width];
+        this.width = width;
+        this.height = height;
+        this.liveRatio = liveRatio;
+        initCurrentState();
+    }
+
+    private void initCurrentState() {
+        for (int y = 0; y < height; y++) {
+            boolean[] row = currentState[y];
+            for (int x = 0; x < width; x++) {
+                double random = Math.random();
+                row[x] = random <= liveRatio;
+            }
+        }
+    }
+
+    public void nextStep() {
+        for (int y = 0; y < height; y++) {
+            boolean[] row = currentState[y];
+            boolean[] newRow = newState[y];
+            for (int x = 0; x < width; x++) {
+                int liveNeighbours = liveNeighbours(x, y);
+                newRow[x] = newCellState(row[x], liveNeighbours);
+            }
+        }
+        swapState();
+    }
+
+    private void swapState() {
+        boolean[][] tmp = this.currentState;
+        this.currentState = newState;
+        this.newState = tmp;
+    }
+
+    private int liveNeighbours(int x, int y) {
+        int ys = Math.max(0, y - 1);
+        int yk = Math.min(y + 1, height - 1);
+        int xs = Math.max(0, x - 1);
+        int xk = Math.min(x + 1, width - 1);
+        int counter = 0;
+        for (int i = ys; i <= yk; i++) {
+            for (int j = xs; j <= xk; j++) {
+                if (currentState[i][j])
+                    counter += 1;
+            }
+        }
+        if (currentState[y][x])
+            counter -= 1;
+        return counter;
+    }
+
+    private boolean newCellState(boolean oldState, int liveNeighbours) {
+        return (oldState && (liveNeighbours == 2 || liveNeighbours == 3))
+                || (!oldState && liveNeighbours == 3);
+    }
+
+    public boolean isAlive(int x, int y) {
+        return currentState[y][x];
+    }
+
+    public void tenCellsRow() {
+        int y = height / 2;
+        int x = width / 2;
+        for (int i = x - 5; i < x + 5; i++)
+            currentState[y][i] = true;
+    }
+}
---
+++ src/pl/com/bottega/life/GameSwingApp.java
@@ -0,0 +1,67 @@
+package pl.com.bottega.documentmanagement.life;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+/**
+ * Created by maciuch on 10.06.16.
+ */
+public class GameSwingApp {
+
+    public static final int W = 80;
+    public static final int H = 60;
+    private JPanel[][] panels;
+    private final GameEngine engine;
+
+    public GameSwingApp() {
+        engine = new GameEngine(W, H, 0);
+        engine.tenCellsRow();
+        showUi();
+        startGame();
+    }
+
+    private void startGame() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                engine.nextStep();
+                for (int i = 0; i < H; i++)
+                    for (int j = 0; j < W; j++) {
+                        color(i, j);
+                    }
+            }
+        }).start();
+    }
+
+    private void showUi() {
+        JFrame f = new JFrame("Game of life");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(new GridLayout(H, W));
+        panels = new JPanel[H][W];
+
+        for (int i = 0; i < H; i++)
+            for (int j = 0; j < W; j++) {
+                panels[i][j] = new JPanel();
+                color(i, j);
+                panels[i][j].setBorder(LineBorder.createGrayLineBorder());
+                f.add(panels[i][j]);
+            }
+        f.setPreferredSize(new Dimension(800, 600));
+        f.setVisible(true);
+        f.pack();
+    }
+
+    private void color(int i, int j) {
+        panels[i][j].setBackground(engine.isAlive(j, i) ? Color.YELLOW : null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GameSwingApp());
+    }
+
+}
